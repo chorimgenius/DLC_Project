dd9d63b37700db64ec11a1c9a9dcd8fa5b26685c
marinred <endrms1995@gmail.com>
2022-11-04 11:02:59 +0900

    feat: Music 모델 설정, views 작성
    - Music models.py  모델링 구현
    - Music views.py 함수 구현
    - Music serializers.py 구현
    
    Close

---
+++ musics/views.py
@@ -0,0 +1,34 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from musics import serializers
+from musics.models import Music
+from musics.serializers import MusicSerializer
+from ML_music.test import recommend_songs
+from review.models import Review
+
+# Create your views here.
+class MusicListView(APIView):
+    def get(self, request):
+        musics = Music.objects.all()
+        serializer = MusicSerializer(musics, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class MusicDetailView(APIView):
+    def get(self, request, music_id):
+        music = get_object_or_404(Music, id=music_id)
+        serializer = MusicSerializer(music)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class MusicLikeView(APIView):
+    def post(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        if request.user in review.likes.all():
+            review.likes.remove(request.user)
+            return Response("Like Cancellation", status=status.HTTP_200_OK)
+        else:
+            review.likes.add(request.user)
+            return Response("Like Complete", status=status.HTTP_200_OK)
+            
